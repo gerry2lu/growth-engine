fad35096234c034416d511225fc8ff3d8d820fc4
Gerry Lu <gerry.lu@sysadmins-MacBook-Pro.local>
2025-02-17 10:30:27 +1100

    Auth Upgrade

--- src/utils/setupDefault.ts
+++ src/utils/setupDefault.ts
@@ -8,8 +8,8 @@ export const passportInstance = new passport.Passport({
       process.env.NEXT_PUBLIC_PUBLISHABLE_KEY || "<YOUR_PUBLISHABLE_KEY>", // replace with your publishable API key from Hub
   },
   clientId: process.env.NEXT_PUBLIC_CLIENT_ID || "<YOUR_CLIENT_ID>", // replace with your client ID from Hub
-  redirectUri: "http://localhost:3000/redirect", // replace with one of your redirect URIs from Hub
-  logoutRedirectUri: "http://localhost:3000", // replace with one of your logout URIs from Hub
+  redirectUri: "https://x-recommendation-engine.vercel.app/redirect", // replace with one of your redirect URIs from Hub
+  logoutRedirectUri: "https://x-recommendation-engine.vercel.app/", // replace with one of your logout URIs from Hub
   audience: "platform_api",
   scope: "openid offline_access email transact",
 });
